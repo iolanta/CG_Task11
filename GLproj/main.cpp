0e16b8cc34005b61b1aa542fa539ae01f62e250f
unknown <iolanta.paly@gmail.com>
2018-11-25 22:06:09 +0300

    add lamp

--- GLproj/main.cpp
+++ GLproj/main.cpp
@@ -17,42 +17,80 @@ void Reshape(int x, int y)
 	h = y;
 }
 
-void draw_wheel(GLfloat x, GLfloat y, GLfloat z, GLfloat of_x, GLfloat of_y, GLfloat of_z) {
+void draw_wheel(GLfloat x, GLfloat y, GLfloat z, int size, GLfloat of_x, GLfloat of_y, GLfloat of_z) {
 	glPushMatrix();
-	glTranslatef(x + of_x, y - of_y, z - of_z);
-	glutSolidTorus(0.03, 0.04, 10, 10);
+	glTranslatef(size*(x + of_x), size*(y + of_y), size*(z + of_z));
+	glutSolidTorus(size * 0.03, size * 0.04, size * 10, size * 10);
 	glPopMatrix();
 }
 
-void draw_light(GLfloat x, GLfloat y, GLfloat z, GLfloat of_x, GLfloat of_y, GLfloat of_z) {
+void draw_light(GLfloat x, GLfloat y, GLfloat z, int size, GLfloat of_x, GLfloat of_y, GLfloat of_z) {
 	glPushMatrix();
-	glTranslatef(x + of_x, y - of_y, z - of_z);
-	glutSolidSphere(0.03, 10, 10);
+	glTranslatef(size * (x + of_x), size * (y + of_y), size * (z + of_z));
+	glutSolidSphere(size * 0.03, size * 10, size * 10);
 	glPopMatrix();
 }
-void draw_car(GLfloat x, GLfloat y, GLfloat z) {
+void draw_car(GLfloat x, GLfloat y, GLfloat z, int size) {
 	glPushMatrix();
 	glTranslatef(x, y, z);
 	glColor3f(1.0f, 0, 0);
-	glutSolidCube(0.3f);
+	glutSolidCube(size * 0.3f);
 
 	glPushMatrix();
 	glColor3f(0, 0, 1.0f);
-	glTranslatef(x + 0.25, y - 0.05, z);
-	glutSolidCube(0.2f);
+	glTranslatef(size * (x + 0.25), size * (y - 0.05), size * z);
+	glutSolidCube(size * 0.2f);
 	glPopMatrix();
 
 	glColor3f(0.0f, 0.0f, 0.0f);
-	draw_wheel(x, y, z, 0.25, 0.15, -0.07);
-	draw_wheel(x, y, z, 0.25, 0.15, 0.07);
-	draw_wheel(x, y, z, -0.08, 0.15, 0.07);
-	draw_wheel(x, y, z, -0.08, 0.15, -0.07);
+	draw_wheel(x, y, z, size, 0.25, -0.15, 0.07);
+	draw_wheel(x, y, z, size, 0.25, -0.15, -0.07);
+	draw_wheel(x, y, z, size, -0.08, -0.15, -0.07);
+	draw_wheel(x, y, z, size, -0.08, -0.15, 0.07);
 
 	glColor3f(1.0f, 1.0f, 0.0f);
-	draw_light(x, y, z, 0.35, 0.1, 0.07);
-	draw_light(x, y, z, 0.35, 0.1, -0.07);
-	draw_light(x, y, z, -0.15, 0.1, 0.09);
-	draw_light(x, y, z, -0.15, 0.1, -0.09);
+	draw_light(x, y, z, size, 0.35, -0.1, -0.07);
+	draw_light(x, y, z, size, 0.35, -0.1, 0.07);
+	draw_light(x, y, z, size, -0.15, -0.1, -0.09);
+	draw_light(x, y, z, size, -0.15, -0.1, 0.09);
+	glPopMatrix();
+}
+
+void draw_lamp(GLfloat x, GLfloat y, GLfloat z, int size, GLfloat angle) {
+	glPushMatrix();
+	glTranslatef(x, y, z);
+	glRotatef(angle, 0.0, 1.0, 0.0);
+
+	glColor3f(0.0f, 0.0f, 0.0f);
+	GLUquadricObj *quadObj = gluNewQuadric();
+	gluQuadricDrawStyle(quadObj, GLU_FILL);
+
+	glPushMatrix();
+	glRotatef(90, 1.0, 0.0, 0.0);
+	gluCylinder(quadObj, size * 0.01, size * 0.01, size * 0.2, size * 15, size * 15);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(size * (x + 0.035), size * (y + 0.03), size * z);
+	glRotatef(90, 1.0, 0.0, 0.0);
+	glRotatef(-45, 0.0, 1.0, 0.0);
+	gluCylinder(quadObj, size * 0.01, size * 0.01, size * 0.05, size * 15, size * 15);
+	glPopMatrix();
+
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glPushMatrix();
+	glTranslatef(size * (x + 0.11), size * (y + 0.025), size * z);
+	glRotatef(-90, 0.0, 1.0, 0.0);
+	gluCylinder(quadObj, size * 0.01, size * 0.01, size * 0.08, size * 15, size * 15);
+	glPopMatrix();
+
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glPushMatrix();
+	glTranslatef(size * (x + 0.12), size * (y + 0.025), size * z);
+	glRotatef(-90, 0.0, 1.0, 0.0);
+	gluCylinder(quadObj, size * 0.01, size * 0.01, size * 0.01, size * 15, size * 15);
+	glPopMatrix();
+
 	glPopMatrix();
 }
 
@@ -64,7 +102,8 @@ void Update(void) {
 	glRotatef(xrotate, 1.0, 0.0, 0.0);
 	glRotatef(yrotate, 0.0, 1.0, 0.0);
 
-	draw_car(0, 0, 0);
+	//draw_car(0, 0, 0, 1);
+	draw_lamp(0, 0, 0, 2, 180);
 	glPopMatrix();
 	glFlush();
 	glutSwapBuffers();
@@ -92,7 +131,6 @@ void keyboard(unsigned char key, int x, int y)
 	}
 
 	glutPostRedisplay();
-
 }
 
 
